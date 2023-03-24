9f32f415a36718997ef4a12da1f0cbc4ae34651d
Saif uddin <dev.saifuddin@gmail.com>
2023-03-24 14:12:57 +0600

    Removed "SideMenu", updated chatlist latest design

--- src/Presentation/components/ChatList/ChatList.js
+++ src/Presentation/components/ChatList/ChatList.js
@@ -13,6 +13,7 @@ import formatTime from "../../../utils/formatTime";
 const ChatList = () => {
   const { chatList, getChatList, chatLoading, handleChat} =
     useGetChatContext();
+    const userInfo =  JSON.parse(localStorage.getItem('user_info'));
 
   useEffect(() => {
     getChatList();
@@ -20,12 +21,16 @@ const ChatList = () => {
 
   return (
     <div className="chat_list_container">
+
       <div className="search_container">
         <div className="search_left">
-          <div>
-            <img src={search_icon} alt="Search_Icon" className="img-fluid" />
+          <div className="profile_img">
+            {userInfo?.first_name?.charAt(0) + userInfo?.last_name?.charAt(0)}
           </div>
-          <Input placeholderText={"Search"} />
+          <div className="user_name">
+            {userInfo?.first_name +" "+ userInfo?.last_name}
+            {/* Saif uddin */}
+          </div>
         </div>
         <button className="add_btn">
           <img src={add_icon} alt="Add_Icon" className="img-fluid" />
@@ -33,79 +38,90 @@ const ChatList = () => {
       </div>
 
       {/* CHAT BOX */}
-      <div className="all_chats_container" style={{display: chatLoading && 'flex', justifyContent: chatLoading && 'center'}}>
-        {chatLoading ? (
-          <div
-            style={{
-              width: "45px",
-              height: "45px",
-            }}
-            className="loader"
-          ></div>
-        ) : (
-          chatList.length > 0 &&
-          chatList.map((chats) => {
-            const { chat_name, chat_id, pinned, latest_msg_list } = chats;
-            return (
-              <div
-                onClick={() => handleChat(chat_id)}
-                className="chat_box_container"
-                key={chat_id}
-              >
-                <div className="chat_box">
-                  <div className="chat_box_left">
-                    <div className="user_avatar_box">
-                      <img
-                        src={user_avatar}
-                        alt="User_Avatar"
-                        className="img-fluid"
-                      />
+      <div className="all_chats_container">
+        <div style={{display: chatLoading && 'flex', justifyContent: chatLoading && 'center'}}>
+          {chatLoading ? (
+            <div
+              style={{
+                width: "45px",
+                height: "45px",
+                marginTop: '20px'
+              }}
+              className="loader"
+            ></div>
+          ) : (
+            chatList.length > 0 &&
+            chatList.map((chats) => {
+              const { chat_name, chat_id, pinned, latest_msg_list } = chats;
+              return (
+                <div
+                  onClick={() => handleChat(chat_id)}
+                  className="chat_box_container"
+                  key={chat_id}
+                >
+                  <div className="chat_box">
+                    <div className="chat_box_left">
+                      {/* <div className="user_avatar_box">
+                        <img
+                          src={user_avatar}
+                          alt="User_Avatar"
+                          className="img-fluid"
+                        />
+                      </div> */}
+                      <div className="content">
+                        <h5 className="user_name">{chat_name}</h5>
+                        <p className="last_text">
+                          {latest_msg_list !== null
+                            ? latest_msg_list[latest_msg_list?.length - 1]
+                                ?.msg_type === "text"
+                              ? latest_msg_list[
+                                  latest_msg_list?.length - 1
+                                ]?.text.slice(0, 20) + "..."
+                              : latest_msg_list[latest_msg_list?.length - 1]
+                                  ?.msg_type === "image"
+                              ? latest_msg_list[latest_msg_list?.length - 1]
+                                  ?.image_name
+                              : latest_msg_list[latest_msg_list?.length - 1]
+                                  ?.msg_type === "file"
+                              ? latest_msg_list[latest_msg_list?.length - 1]
+                                  ?.file_name
+                              : "No Messages" : 'No Messages'
+                            }
+                        </p>
+                      </div>
                     </div>
-                    <div className="content">
-                      <h5 className="user_name">{chat_name}</h5>
-                      <p className="last_text">
+                    <div className="chat_box_right">
+                      <p className="date_time">
                         {latest_msg_list !== null
-                          ? latest_msg_list[latest_msg_list?.length - 1]
-                              ?.msg_type === "text"
-                            ? latest_msg_list[
-                                latest_msg_list?.length - 1
-                              ]?.text.slice(0, 20) + "..."
-                            : latest_msg_list[latest_msg_list?.length - 1]
-                                ?.msg_type === "image"
-                            ? latest_msg_list[latest_msg_list?.length - 1]
-                                ?.image_name
-                            : latest_msg_list[latest_msg_list?.length - 1]
-                                ?.msg_type === "file"
-                            ? latest_msg_list[latest_msg_list?.length - 1]
-                                ?.file_name
-                            : "No Messages" : 'No Messages'
-                          }
+                          ? formatTime(
+                              latest_msg_list[latest_msg_list.length - 1]
+                                ?.backend_utc_timestamp
+                            )
+                          : "00:00"}
                       </p>
-                    </div>
-                  </div>
-                  <div className="chat_box_right">
-                    <p className="date_time">
-                      {latest_msg_list !== null
-                        ? formatTime(
-                            latest_msg_list[latest_msg_list.length - 1]
-                              ?.backend_utc_timestamp
-                          )
-                        : "00:00"}
-                    </p>
-                    <div>
-                      <img
-                        src={pinned ? pin_filled_icon : pin_icon}
-                        alt="pin_icon"
-                        className="img-fluid pin_icon"
-                      />
+                      <div>
+                        <img
+                          src={pinned ? pin_filled_icon : pin_icon}
+                          alt="pin_icon"
+                          className="img-fluid pin_icon"
+                        />
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            );
-          })
-        )}
+              );
+            })
+          )}
+        </div>
+        <div className="chats_bottom">
+        <div className="chat_bottom_items">credit left</div>
+        <div className="chat_bottom_items">Upgrade now</div>
+        <div className="chat_bottom_items">Invite a friend</div>
+        <div className="chat_bottom_items">Settings</div>
+        <div className="chat_bottom_items">Logout</div>
       </div>
+      </div>
+
     </div>
   );
 };
