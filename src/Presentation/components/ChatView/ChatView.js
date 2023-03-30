55e2020abb348e8c3b5e53f9821cbffce19bb2eb
Saif Uddin <shyboy.skk@gmail.com>
2023-03-31 03:31:51 +0600

    removed unused vars

--- src/Presentation/components/ChatView/ChatView.js
+++ src/Presentation/components/ChatView/ChatView.js
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect } from "react";
+import { useRef, useEffect } from "react";
 import ChatInput from "../ChatInput/ChatInput";
 import ChatViewHeader from "./ChatViewHeader";
 import "./ChatView.css";
